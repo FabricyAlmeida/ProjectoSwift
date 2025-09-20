79b03bc070149e82837ebddec55c39cc878f4473
Fabricy Almeida <fabrialmeraa@gmail.com>
2025-09-20 23:04:26 +0100

    Primeiro Teste

---
+++ Timer Swif/Timer Swif/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timer Swif
+//
+//  Created by fabrialmeraa_90@icloud.com on 20/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Timer Swif/Timer SwifTests/Timer_SwifTests.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_SwifTests.swift
+//  Timer SwifTests
+//
+//  Created by fabrialmeraa_90@icloud.com on 20/09/25.
+//
+
+import Testing
+@testable import Timer_Swif
+
+struct Timer_SwifTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
